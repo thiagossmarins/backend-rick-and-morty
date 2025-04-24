d6e8a98fb5f82b36ca1b5f041a463cd1e841b613
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2025-04-23 23:30:33 -0300

    Applying initial structure

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_wold():
+  return jsonify({
+    "hello": "wold"
+  }
+  )
+
+if __name__ == "__main__":
+  app.run(debug=True)
