6d1599b7b37a759755f3fe4fac21a079b0b436d0
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2025-04-24 20:59:14 -0300

    Config sql alchemy

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify
+from src.models import db
+from config.settings import DATABASE_URI
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/')
 def hello_wold():
