ca3167861f62cc1541309e1d9c04f0dce55da443
dev-thiago <dev-thiago@Mac-Pro-de-dev-thiago.local>
2025-05-14 20:18:37 -0300

    Implementing ma on model

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from src.models import db
+from src.models import db, ma
 from config.settings import DATABASE_URI
 from src.routes.characters_routes import characters_bp
 
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+ma.init_app(app)
 
 app.register_blueprint(characters_bp, url_prefix='/characters')
 
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
+ma = Marshmallow()
+
+from src.models.characters_model import Characters
